bed2ab2e195111e80888799f6bc332b86699b1ae
xczlb <131801077+xczlb@users.noreply.github.com>
2023-05-06 11:44:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,22 +39,22 @@ import img5 from "./assets/images/5.png";
 import img6 from "./assets/images/6.png";
 import img7 from "./assets/images/7.png";
 import img8 from "./assets/images/8.png";
-import img1 from "./assets/images/9.png";
-import img2 from "./assets/images/10.png";
-import img3 from "./assets/images/11.png";
-import img4 from "./assets/images/12.png";
-import img5 from "./assets/images/13.png";
-import img6 from "./assets/images/14.png";
-import img7 from "./assets/images/15.png";
-import img8 from "./assets/images/16.png";
-import img1 from "./assets/images/17.png";
-import img2 from "./assets/images/18.png";
-import img3 from "./assets/images/19.png";
-import img4 from "./assets/images/20.png";
-import img5 from "./assets/images/21.png";
-import img6 from "./assets/images/22.png";
-import img7 from "./assets/images/23.png";
-import img8 from "./assets/images/24.png";
+import img9 from "./assets/images/9.png";
+import img10 from "./assets/images/10.png";
+import img11 from "./assets/images/11.png";
+import img12 from "./assets/images/12.png";
+import img13 from "./assets/images/13.png";
+import img14 from "./assets/images/14.png";
+import img15 from "./assets/images/15.png";
+import img16 from "./assets/images/16.png";
+import img17 from "./assets/images/17.png";
+import img18 from "./assets/images/18.png";
+import img19 from "./assets/images/19.png";
+import img20 from "./assets/images/20.png";
+import img21 from "./assets/images/21.png";
+import img22 from "./assets/images/22.png";
+import img23 from "./assets/images/23.png";
+import img24 from "./assets/images/24.png";
 
 export default {
   components: {
