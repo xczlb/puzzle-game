809e25a6402d9faa9c13f562fd755502417887d8
xczlb <131801077+xczlb@users.noreply.github.com>
2023-05-06 11:39:11 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,22 @@ import img5 from "./assets/images/5.png";
 import img6 from "./assets/images/6.png";
 import img7 from "./assets/images/7.png";
 import img8 from "./assets/images/8.png";
+import img1 from "./assets/images/9.png";
+import img2 from "./assets/images/10.png";
+import img3 from "./assets/images/11.png";
+import img4 from "./assets/images/12.png";
+import img5 from "./assets/images/13.png";
+import img6 from "./assets/images/14.png";
+import img7 from "./assets/images/15.png";
+import img8 from "./assets/images/16.png";
+import img1 from "./assets/images/17.png";
+import img2 from "./assets/images/18.png";
+import img3 from "./assets/images/19.png";
+import img4 from "./assets/images/20.png";
+import img5 from "./assets/images/21.png";
+import img6 from "./assets/images/22.png";
+import img7 from "./assets/images/23.png";
+import img8 from "./assets/images/24.png";
 
 export default {
   components: {
